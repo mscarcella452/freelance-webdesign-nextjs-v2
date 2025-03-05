96bc1a7578fd3d9be993d728452aeb358618c1f1
Matt Scarcella <mscarcella452@gmail.com>
2025-03-05 15:46:15 -0500

    Fixed problem with tailwind css layers

--- features/HeroSection.tsx
+++ features/HeroSection.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 function HeroSection() {
-  return (
-    <section className='w-full  bg-blue-500'>
-      <div className=' @container padding-container  h-[500px] w-full '>
-        <div className='bg-red-500 mx-auto w-full h-full'></div>
-      </div>
-    </section>
-  );
+  return <section className='p-hero w-full'></section>;
 }
 
 export default HeroSection;
